0da00e33062b34dd0ad902caed71fe10421ee615
Jefferson-F-Ribeiro <93939600+Jefferson-F-Ribeiro@users.noreply.github.com>
2023-09-03 20:58:32 -0300

    Implementa randomicidade total na exclusividade

--- java/passwordgenerator/generator.java
+++ java/passwordgenerator/generator.java
@@ -1,5 +1,8 @@
 import java.util.Random;
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Collections;
 
 public class generator {
 
@@ -59,27 +62,62 @@ public class generator {
 
         public String exclusiveGeneratePassword(){
             Random r = new Random();
-
-            char upperCaseChar = this.charactersUpperCase.getCharacters().charAt(r.nextInt(this.charactersUpperCase.getCharacters().length()));
-            char symbolChar = this.charactersSymbols.getCharacters().charAt(r.nextInt(this.charactersSymbols.getCharacters().length()));
-            char loweCaseChar = this.charactersLowerCase.getCharacters().charAt(r.nextInt(this.charactersLowerCase.getCharacters().length()));
-            char numbersChar = this.charactersNumbers.getCharacters().charAt(r.nextInt(this.charactersNumbers.getCharacters().length()));
-
-            StringBuilder password = new StringBuilder();
-            
-            if(this.length > 4){
-                for(int i=0; i<this.length-2; i++){
-                    int index = r.nextInt(this.customSelection.getCharacters().length());
-                    password.append(this.customSelection.getCharacters().charAt(index));
-                }
+        
+            List<Character> upperCaseChars = new ArrayList<>();
+            List<Character> symbolChars = new ArrayList<>();
+            List<Character> lowerCaseChars = new ArrayList<>();
+            List<Character> numberChars = new ArrayList<>();
+        
+            for (char c : this.charactersUpperCase.getCharacters().toCharArray()) {
+                upperCaseChars.add(c);
             }
-
-            password.insert(r.nextInt(password.length() + 1), upperCaseChar);
-            password.insert(r.nextInt(password.length() + 1), symbolChar);
-            password.insert(r.nextInt(password.length() + 1), loweCaseChar);
-            password.insert(r.nextInt(password.length() + 1), numbersChar);
-
-            return password.toString();
+            for (char c : this.charactersSymbols.getCharacters().toCharArray()) {
+                symbolChars.add(c);
+            }
+            for (char c : this.charactersLowerCase.getCharacters().toCharArray()) {
+                lowerCaseChars.add(c);
+            }
+            for (char c : this.charactersNumbers.getCharacters().toCharArray()) {
+                numberChars.add(c);
+            }
+        
+            Collections.shuffle(upperCaseChars);
+            Collections.shuffle(symbolChars);
+            Collections.shuffle(lowerCaseChars);
+            Collections.shuffle(numberChars);
+        
+            List<Character> masterList = new ArrayList<>();
+            masterList.addAll(upperCaseChars);
+            masterList.addAll(symbolChars);
+            masterList.addAll(lowerCaseChars);
+            masterList.addAll(numberChars);
+        
+            Collections.shuffle(masterList);
+        
+            StringBuilder password = new StringBuilder();
+        
+            password.append(upperCaseChars.get(0));
+            password.append(symbolChars.get(0));
+            password.append(lowerCaseChars.get(0));
+            password.append(numberChars.get(0));
+        
+            for (int i = 4; i < this.length; i++) {
+                int index = r.nextInt(this.customSelection.getCharacters().length());
+                password.append(this.customSelection.getCharacters().charAt(index));
+            }
+        
+            List<Character> passwordList = new ArrayList<>();
+            for (char c : password.toString().toCharArray()) {
+                passwordList.add(c);
+            }
+            Collections.shuffle(passwordList);
+            
+            StringBuilder randomizedPassword = new StringBuilder();
+            for (char c : passwordList) {
+                randomizedPassword.append(c);
+            }
+        
+            return randomizedPassword.toString();
         }
 
         public String customGenerator(String a, String b, String c, String d){
