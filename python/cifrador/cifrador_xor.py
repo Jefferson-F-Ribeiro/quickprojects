5ea744a10110cd9a147e9c84f8fd2e701b885c34
Jefferson-F-Ribeiro <93939600+Jefferson-F-Ribeiro@users.noreply.github.com>
2023-09-07 06:00:11 -0300

    Remove a geração com caracteres não imprimíveis

--- python/cifrador/cifrador_xor.py
+++ python/cifrador/cifrador_xor.py
@@ -1,38 +1,39 @@
-def gerar_chave():
-    # Gere uma chave simples (deve ter o mesmo comprimento que o texto)
-    chave = "minhachave"
-    return chave
-
 def cifrar(texto, chave):
     texto_cifrado = ""
     for i in range(len(texto)):
         char_original = texto[i]
         char_chave = chave[i % len(chave)]  # Repetir a chave se for mais curta que o texto
         resultado = ord(char_original) ^ ord(char_chave)  # Operação XOR
-        texto_cifrado += chr(resultado)
+        texto_cifrado += chr(resultado + 32)  # Adicionar 32 para garantir que o resultado seja imprimível
     return texto_cifrado
 
 def decifrar(texto_cifrado, chave):
     texto_decifrado = ""
     for i in range(len(texto_cifrado)):
         char_cifrado = texto_cifrado[i]
-        char_chave = chave[i % len(chave)]  # Repetir a chave se for mais curta que o texto
-        resultado = ord(char_cifrado) ^ ord(char_chave)  # Operação XOR
+        char_chave = chave[i % len(chave)]  # Repetir a chave se for mais curta que o texto cifrado
+        resultado = (ord(char_cifrado) - 32) ^ ord(char_chave)  # Subtrair 32 para desfazer a adição na cifragem
         texto_decifrado += chr(resultado)
     return texto_decifrado
 
 def main():
     print("Cifrador XOR (ou exclusivo)")
 
-    chave = gerar_chave()
+    chave = "chave123"  # Certifique-se de que a chave consista em caracteres imprimíveis
     print("Chave gerada:", chave)
 
-    texto_original = input("Digite o texto a ser cifrado: ")
+    texto_original = input("Digite o texto a ser cifrado (somente caracteres imprimíveis): ")
+
+    # Remova caracteres que não sejam imprimíveis
+    texto_original = ''.join(filter(lambda x: 32 <= ord(x) <= 126, texto_original))
+
+    # Ajuste o tamanho da chave para corresponder ao texto original
+    chave = chave * (len(texto_original) // len(chave)) + chave[:len(texto_original) % len(chave)]
 
     texto_cifrado = cifrar(texto_original, chave)
-    print("Texto cifrado:", texto_cifrado)
-
     texto_decifrado = decifrar(texto_cifrado, chave)
+
+    print("Texto cifrado:", texto_cifrado)
     print("Texto decifrado:", texto_decifrado)
 
 if __name__ == "__main__":
