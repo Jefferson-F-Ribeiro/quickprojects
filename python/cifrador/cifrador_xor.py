c0c62b7957364f5338d112b44c5919868434dba3
Jefferson-F-Ribeiro <93939600+Jefferson-F-Ribeiro@users.noreply.github.com>
2023-09-08 16:44:10 -0300

    Modifica a chave pelo input do usuário

--- python/cifrador/cifrador_xor.py
+++ python/cifrador/cifrador_xor.py
@@ -2,32 +2,28 @@ def cifrar(texto, chave):
     texto_cifrado = ""
     for i in range(len(texto)):
         char_original = texto[i]
-        char_chave = chave[i % len(chave)]  # Repetir a chave se for mais curta que o texto
-        resultado = ord(char_original) ^ ord(char_chave)  # Operação XOR
-        texto_cifrado += chr(resultado + 32)  # Adicionar 32 para garantir que o resultado seja imprimível
+        char_chave = chave[i % len(chave)]
+        resultado = ord(char_original) ^ ord(char_chave)
+        texto_cifrado += chr(resultado + 32)
     return texto_cifrado
 
 def decifrar(texto_cifrado, chave):
     texto_decifrado = ""
     for i in range(len(texto_cifrado)):
         char_cifrado = texto_cifrado[i]
-        char_chave = chave[i % len(chave)]  # Repetir a chave se for mais curta que o texto cifrado
-        resultado = (ord(char_cifrado) - 32) ^ ord(char_chave)  # Subtrair 32 para desfazer a adição na cifragem
+        char_chave = chave[i % len(chave)]
+        resultado = (ord(char_cifrado) - 32) ^ ord(char_chave)
         texto_decifrado += chr(resultado)
     return texto_decifrado
 
 def main():
     print("Cifrador XOR (ou exclusivo)")
 
-    chave = "chave123"  # Certifique-se de que a chave consista em caracteres imprimíveis
-    print("Chave gerada:", chave)
-
+    chave = input("Digite a chave (somente caracteres imprimíveis): ")
     texto_original = input("Digite o texto a ser cifrado (somente caracteres imprimíveis): ")
 
-    # Remova caracteres que não sejam imprimíveis
     texto_original = ''.join(filter(lambda x: 32 <= ord(x) <= 126, texto_original))
 
-    # Ajuste o tamanho da chave para corresponder ao texto original
     chave = chave * (len(texto_original) // len(chave)) + chave[:len(texto_original) % len(chave)]
 
     texto_cifrado = cifrar(texto_original, chave)
