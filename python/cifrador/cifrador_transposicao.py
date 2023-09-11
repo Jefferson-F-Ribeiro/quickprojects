885fed24f587b9221e2c81d195c7853e5d9b108f
Jefferson-F-Ribeiro <93939600+Jefferson-F-Ribeiro@users.noreply.github.com>
2023-09-11 15:13:17 -0300

    Implementa o decifrador transposição

--- python/cifrador/cifrador_transposicao.py
+++ python/cifrador/cifrador_transposicao.py
@@ -18,3 +18,28 @@ def cifrar_transposicao(texto, chave):
             texto_cifrado += matriz[linha][coluna_idx]
 
     return texto_cifrado
+
+def decifrar_transposicao(texto_cifrado, chave):
+    num_colunas = len(chave)
+    num_linhas = len(texto_cifrado) // num_colunas + int(len(texto_cifrado) % num_colunas != 0)
+
+    coluna_len = [len(texto_cifrado) // num_colunas] * num_colunas
+    for i in range(len(texto_cifrado) % num_colunas):
+        coluna_len[i] += 1
+
+    matriz = [[' ' for _ in range(num_colunas)] for _ in range(num_linhas)]
+
+    index = 0
+    for coluna in chave:
+        coluna_idx = int(coluna) - 1
+        for linha in range(num_linhas):
+            if index < len(texto_cifrado):
+                matriz[linha][coluna_idx] = texto_cifrado[index]
+                index += 1
+
+    texto_decifrado = ""
+    for linha in range(num_linhas):
+        for coluna in range(num_colunas):
+            texto_decifrado += matriz[linha][coluna]
+
+    return texto_decifrado
