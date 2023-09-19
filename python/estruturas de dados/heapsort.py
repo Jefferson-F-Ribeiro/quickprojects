f9c635e2609802d36d14681fe0da1fd05dc88864
Jefferson-F-Ribeiro <93939600+Jefferson-F-Ribeiro@users.noreply.github.com>
2023-09-19 01:02:27 -0300

    Implementa o Heapsort

---
+++ python/estruturas de dados/heapsort.py
@@ -0,0 +1,53 @@
+import time
+
+def heapify(arr, n, i):
+    largest = i
+    left = 2 * i + 1
+    right = 2 * i + 2
+
+    if left < n and arr[left] > arr[largest]:
+        largest = left
+
+    if right < n and arr[right] > arr[largest]:
+        largest = right
+
+    if largest != i:
+        arr[i], arr[largest] = arr[largest], arr[i]
+        heapify(arr, n, largest)
+
+def heap_sort(arr):
+    n = len(arr)
+
+    for i in range(n // 2 - 1, -1, -1):
+        heapify(arr, n, i)
+
+    for i in range(n - 1, 0, -1):
+        arr[i], arr[0] = arr[0], arr[i]
+        heapify(arr, i, 0)
+
+def main():
+    try:
+        n = int(input("Digite o número de elementos no array: "))
+        arr = []
+        for i in range(n):
+            element = int(input(f"Digite o elemento {i + 1}: "))
+            arr.append(element)
+
+        print("Array original:")
+        print(arr)
+
+        start_time = time.time()
+        heap_sort(arr)
+        end_time = time.time()
+
+        print("Array ordenado:")
+        print(arr)
+
+        elapsed_time = end_time - start_time
+        print(f"Tempo de ordenação: {elapsed_time:.6f} segundos")
+
+    except ValueError:
+        print("Por favor, digite números inteiros válidos.")
+
+if __name__ == "__main__":
+    main()
