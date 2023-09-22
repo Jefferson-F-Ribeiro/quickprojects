4d32410bf5f2db607ad8e8c52c5e7584a99f8d4e
Jefferson-F-Ribeiro <93939600+Jefferson-F-Ribeiro@users.noreply.github.com>
2023-09-22 00:59:01 -0300

    Adiciona mais 4 casos de teste do bubblesort

--- python/estruturas de dados/bubblesort.py
+++ python/estruturas de dados/bubblesort.py
@@ -1,4 +1,5 @@
 import time
+import random
 
 def bubble_sort(arr):
     n = len(arr)
@@ -35,6 +36,9 @@ class BubbleSortTestCase:
         print(f"Tempo de ordenação: {elapsed_time:.6f} segundos")
         print("\n")
 
+def generate_random_array(size):
+    return [random.randint(1, 1000) for _ in range(size)]
+
 def main():
     # Caso de Teste 1: Array já ordenado em ordem crescente.
     test_case_1 = BubbleSortTestCase("Array Crescente", [1, 2, 3, 4, 5], bubble_sort)
@@ -48,11 +52,30 @@ def main():
     # Caso de Teste 4: Array com elementos repetidos.
     test_case_4 = BubbleSortTestCase("Array com Repetição", [3, 2, 1, 2, 3, 1], bubble_sort)
 
+    # Caso de Teste 5: Array já ordenado em ordem crescente (tamanho 50).
+    test_case_5 = BubbleSortTestCase("Array Crescente 50", list(range(1, 51)), bubble_sort)
+
+    # Caso de Teste 6: Array já ordenado em ordem decrescente (tamanho 50).
+    test_case_6 = BubbleSortTestCase("Array Decrescente 50", list(range(50, 0, -1)), bubble_sort)
+
+    # Caso de Teste 7: Array com elementos aleatórios (tamanho 50).
+    random_array = generate_random_array(50)
+    test_case_7 = BubbleSortTestCase("Array Aleatório 50", random_array, bubble_sort)
+
+    # Caso de Teste 8: Array com elementos repetidos (tamanho 50).
+    repeated_array = [random.randint(1, 10) for _ in range(50)]
+    test_case_8 = BubbleSortTestCase("Array com Repetição 50", repeated_array, bubble_sort)
+
+
     # Executar os casos de teste
     test_case_1.run()
     test_case_2.run()
     test_case_3.run()
     test_case_4.run()
+    test_case_5.run()
+    test_case_6.run()
+    test_case_7.run()
+    test_case_8.run()
 
 if __name__ == "__main__":
     main()
