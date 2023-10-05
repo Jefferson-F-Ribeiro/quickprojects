db7b29277f94d4ad49249c0fbbc9d5f2668fab93
Jefferson-F-Ribeiro <93939600+Jefferson-F-Ribeiro@users.noreply.github.com>
2023-10-05 01:04:56 -0300

    Adiciona interação do usuário

--- python/estruturas de dados/pilha.py
+++ python/estruturas de dados/pilha.py
@@ -23,14 +23,45 @@ class Pilha:
     def tamanho(self):
         return len(self.items)
 
+    def imprimir(self):
+        print("Elementos da pilha:", self.items)
+
+def exibir_menu():
+    print("\nMenu:")
+    print("1. Push (Adicionar elemento)")
+    print("2. Pop (Remover elemento)")
+    print("3. Topo (Consultar o elemento no topo)")
+    print("4. Imprimir toda a pilha")
+    print("5. Sair")
+
 if __name__ == "__main__":
     pilha = Pilha()
 
-    pilha.push(1)
-    pilha.push(2)
-    pilha.push(3)
+    while True:
+        exibir_menu()
+        escolha = input("Escolha uma opção: ")
 
-    print("Elementos da pilha:")
-    while not pilha.vazia():
-        print(pilha.pop())
+        if escolha == "1":
+            elemento = input("Digite o elemento a ser adicionado: ")
+            pilha.push(elemento)
+            print(f"{elemento} foi adicionado à pilha.")
+        elif escolha == "2":
+            if not pilha.vazia():
+                elemento_removido = pilha.pop()
+                print(f"{elemento_removido} foi removido da pilha.")
+            else:
+                print("A pilha está vazia.")
+        elif escolha == "3":
+            if not pilha.vazia():
+                elemento_topo = pilha.topo()
+                print(f"O elemento no topo da pilha é: {elemento_topo}")
+            else:
+                print("A pilha está vazia.")
+        elif escolha == "4":
+            pilha.imprimir()
+        elif escolha == "5":
+            break
+        else:
+            print("Escolha uma opção válida.")
 
+    print("Programa encerrado.")
